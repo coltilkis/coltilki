550ca97b3ba6298ccb220b263d31f50125d00a06
coltilkis <tolcilkis@gmail.com>
2024-07-18 15:44:37 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find -type f -name '*.php' | while read file; do
-  release_file=$(echo "$file" | sed -e 's/\.php$/.html/')
-  currentdir=$(pwd)
-  mkdir -p "$(dirname "$release_file")" && \
-  (cd "$(dirname "$file")" && php -f "$(basename "$file")" > "$currentdir/$(dirname "$release_file")/$(basename "$release_file")")
-done
